8beab88de9c869a91b46acb5c3aa80fba339f79e
Kratos <filipmikolajgabriel@gmail.com>
2020-07-23 20:25:24 +0200

    Improved version

--- src/NonrepeatingCharacter.java
+++ src/NonrepeatingCharacter.java
@@ -7,37 +7,24 @@ class Main {
 
         String[] character = str.split("");
         int length = str.length();
-        int repeated = 0;
 
-        for(int i = 0; i < length; i++)
-        {
-            for(int j = 0; j < length; j++)
-            {
-                if(i != j) {
-                    System.out.print("i = " + i + " letter = " + character[i] + " | ");
-                    System.out.println("j = " + j + " letter = " + character[j]);
-                }
-
-                if (character[i].equals(character[j]) && i != j)
-                {
-                    str = "There are not nonrepeat character";
-                    System.out.println(str);
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (character[i].equals(character[j]) && i != j) {
+                    str = "No results";
                     break;
-                }
-                else if (!character[i].equals(character[j]) && j == length - 1)
-                {
-                    str = character[i];
+                } else if (j == length - 1) {
+                    return str = character[i];
                 }
             }
         }
         return str;
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // keep this function call here
         System.out.print("Enter word: ");
         Scanner s = new Scanner(System.in);
-        System.out.print("First non repeat letter is: " + NonrepeatingCharacter(s.nextLine()));
+        System.out.print("First non repeated letter is: " + NonrepeatingCharacter(s.nextLine()));
     }
-
 }
